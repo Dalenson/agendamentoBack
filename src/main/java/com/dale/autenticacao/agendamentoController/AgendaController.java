111bcb6c4eee998322423d12e72f2907d5c97dd5
Dale <dal.colares@gmail.com>
2023-06-25 18:24:35 -0300

    Melhorias

--- src/main/java/com/dale/autenticacao/agendamentoController/AgendaController.java
+++ src/main/java/com/dale/autenticacao/agendamentoController/AgendaController.java
@@ -4,6 +4,8 @@ import com.dale.autenticacao.agendamentoService.AgendamentoService;
 import com.dale.autenticacao.angendamentoModel.Agenda;
 import com.dale.autenticacao.angendamentoModel.DadosUserDTO;
 import com.dale.autenticacao.autService.UsuarioService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/agenda")
 public class AgendaController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AgendaController.class);
 
     @Autowired
     private AgendamentoService agendamentoService;
@@ -25,11 +29,13 @@ public class AgendaController {
 
     @GetMapping("/listaAgendamentos")
     public List<Agenda> listaAgendamentos(@RequestHeader("Authorization") String token){
+        LOG.info("Buscando lista de agendamentos");
         return agendamentoService.listaAgendamentosPorUsuario(token);
     }
 
     @PostMapping("/adicionarAgendamento")
     public Agenda adicionarAgendamento(@RequestBody Agenda agendamento, @RequestHeader("Authorization") String token, @RequestHeader("DiasSemana") Boolean DiasSemana){
+        LOG.info("Criando agendamento");
         Agenda agenda = agendamentoService.adicionarAgendamento(agendamento, token, DiasSemana);
         messagingTemplate.convertAndSend("/topic/messages", agendamentoService.listaAgendamentosPorUsuario(token));
         return agenda;
@@ -37,11 +43,13 @@ public class AgendaController {
 
     @DeleteMapping("/excluirAgendamento/{id}")
     public void excluirAgenda(@PathVariable Long id){
+        LOG.info("Excluindo agendamento");
         agendamentoService.excluirAgendamento(id);
     }
 
     @GetMapping("/buscaDadosUsuario")
     public DadosUserDTO buscaDadosUsuario(@RequestHeader("Authorization") String token){
+        LOG.info("Buscando dados do usuario");
         return usuarioService.buscaDadosUsuario(token);
     }
 }
